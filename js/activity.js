c1971fee31333d8b2d20c94665f56a2a1edcc4e9
Yaroslav Hamuha <yhamuha@gmail.com>
2019-05-28 15:25:47 -0700

    Update activity.js

--- js/activity.js
+++ js/activity.js
@@ -4,8 +4,8 @@
 function activityCtrl ($scope) {
     $scope.activities = [
         {
-            info: 'Should',
-            name: '@thletics',
+            info: 'Love',
+            name: '@running',
             href: 'https://runkeeper.com/user/neutralpoint/profile'
         },
         {
@@ -19,7 +19,7 @@ function activityCtrl ($scope) {
             href: 'https://yhamuha.github.io/neutral_point_interview_en.pdf'
         },
         {
-            info: 'Relax',
+            info: 'Sometime',
             name: '@hess ',
             href: 'https://account.chessbase.com/en'
         }
