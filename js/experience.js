0bb4a9bb344d983bd8157c74617393262aca1a9d
Yaroslav Hamuha <yhamuha@gmail.com>
2023-08-14 08:14:33 -0700

    Update experience.js

--- js/experience.js
+++ js/experience.js
@@ -4,29 +4,43 @@
 function experienceCtrl ($scope) {
     $scope.experiences = [
         {
-            dateRange: 'Apr 2019 - today',
-            position: 'S-CONSULTING',
+            dateRange: 'Dec 2021 - current time',
+            position: 'Software Engineer II -> AMEX',
+            state: '',
+            role: 'Keen on Java, JS, Spring Boot/MVC, REST , RDBMS, MongoDB, Docker, AWS.',
+            href: ''
+        },
+        {
+            dateRange: 'Jan 2021 - Sep 2021',
+            position: 'Network Software Engineer -> MICROSOFT',
+            state: '',
+            role: 'Keen on Java, JS, Spring Boot/MVC, REST , RDBMS, MongoDB, Docker, AWS.',
+            href: ''
+        },
+        {
+            dateRange: 'Apr 2019 - Jan 2020',
+            position: 'Software Engineer -> S-CONSULTING',
             state: '',
             role: 'Keen on Java, JS, Spring Boot/MVC, REST , RDBMS, MongoDB, Docker, AWS.',
             href: ''
         },
         {
             dateRange: 'May 2018 - Feb 2019',
-            position: 'Software Engineer on USEL',
+            position: 'Software Engineer -> USEL',
             state: '',
             role: 'Have done significant amount of work on the REST-based (OAS 3.0) project from scratch that provide services regarding internal tasks. Provided domain decomposition, requirement analysis, system design, implementation and testing by SDLC. Worked using TDD and SOLID approaches, GRASP patterns. Created a business logic utilized JavaEE on back-end, MySQL DB, Spring Data/MVC/Boot, Hibernate. Used Docker, Gradle, Eclipse STS, Git, FlyWay data migration tool, Swagger, Postman for endpoint tests.',
             href: ''
         },
         {
             dateRange: 'Jun 2015 - May 2018',
-            position: 'Java Backend Developer on BECOME JAVA SENIOR',
+            position: 'Java Backend Developer -> BECOME JAVA SENIOR',
             state: '',
             role: 'Worked on improvment technical skills with Senior Java mentor and worked on the project to develop separate functionalities of B2B solution that provide a services for Real Estate. Used data structures utilized Java Collections API, JDBC, DAO, Servlets, JSP, JSTL, SQL. Used Tomcat Web Server, Maven, Git.',
             href: 'https://www.linkedin.com/company/becomejavasenior/'
         },
         {
             dateRange: 'Dec 2014 - Mar 2015',
-            position: 'Junior Web Developer on SOFT SERVE INC',
+            position: 'Junior Web Developer -> SOFT SERVE INC',
             state: '',
             role: 'Trained in developing a part of layer of web application for municipal area. Took participation in developing back-end on Java, Spring MVC, Servlets, SQL, as well as front-end on HTML5, CSS3, BootStrap. Used Scrum Agile, Maven, Git, Google Map API.',
             href: 'https://www.linkedin.com/company/softserve-business-systems/'
